d676ab960d420812b3550be2576cc7ce73621318
Carlos Costa <carllos.nc@gmail.com>
2026-05-23 01:39:13 -0300

    feat(authentication): add authentication types

---
+++ src/types/authentication.ts
@@ -0,0 +1,24 @@
+export interface GuestSession {
+  success: boolean;
+  guest_session_id: string;
+  expires_at: string;
+}
+
+export interface RequestToken {
+  success: boolean;
+  expires_at: string;
+  request_token: string;
+}
+
+export interface CreateSessionRequest {
+  request_token: string;
+}
+
+export interface Session {
+  success: boolean;
+  session_id: string;
+}
+
+export interface CreateSessionFromV4TokenRequest {
+  access_token: string;
+}
