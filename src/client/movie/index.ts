f6996f21573d69e41aac676eb7a29b4f6e0019b9
Carlos Costa <carllos.nc@gmail.com>
2026-05-23 14:19:48 -0300

    feat: implement all movie endpoint methods and types

--- src/client/movie/index.ts
+++ src/client/movie/index.ts
@@ -5,6 +5,35 @@ import {
   type PopularParams,
   type TopRatedParams,
   type UpcomingParams,
+  type MovieDetails,
+  type MovieDetailsParams,
+  type MovieAccountStates,
+  type MovieAccountStatesParams,
+  type MovieAlternativeTitlesResponse,
+  type MovieAlternativeTitlesParams,
+  type MovieChangesResponse,
+  type MovieChangesParams,
+  type MovieCreditsResponse,
+  type MovieCreditsParams,
+  type MovieExternalIdsResponse,
+  type MovieImagesResponse,
+  type MovieImagesParams,
+  type MovieKeywordsResponse,
+  type MovieListsResponse,
+  type MovieListsParams,
+  type MovieRecommendationsParams,
+  type MovieReleaseDatesResponse,
+  type MovieSimilarParams,
+  type MovieReviewsResponse,
+  type MovieReviewsParams,
+  type MovieTranslationsResponse,
+  type MovieVideosResponse,
+  type MovieVideosParams,
+  type MovieWatchProvidersResponse,
+  type AddRatingRequest,
+  type AddRatingParams,
+  type DeleteRatingParams,
+  type TMDBResponse,
 } from "../../types/movie.ts";
 
 export class MovieClient {
@@ -63,6 +92,253 @@ export class MovieClient {
     if (params?.region) queryParams["region"] = params.region;
 
     const response = await this.axiosInstance.get("movie/upcoming", { params: queryParams });
+    return response.data;
+  }
+
+  /**
+   * Get the top level details of a movie by ID.
+   * @see https://developer.themoviedb.org/reference/movie-details
+   */
+  async getDetails(id: number, params?: MovieDetailsParams): Promise<MovieDetails> {
+    const queryParams: Record<string, any> = {};
+    if (params?.append_to_response) queryParams["append_to_response"] = params.append_to_response;
+    if (params?.language) queryParams["language"] = params.language;
+
+    const response = await this.axiosInstance.get(`movie/${id}`, { params: queryParams });
+    return response.data;
+  }
+
+  /**
+   * Get the rating, watchlist and favourite status of an account.
+   * @see https://developer.themoviedb.org/reference/movie-account-states
+   */
+  async getAccountStates(
+    id: number,
+    params?: MovieAccountStatesParams
+  ): Promise<MovieAccountStates> {
+    const queryParams: Record<string, any> = {};
+    if (params?.session_id) queryParams["session_id"] = params.session_id;
+    if (params?.guest_session_id) queryParams["guest_session_id"] = params.guest_session_id;
+
+    const response = await this.axiosInstance.get(`movie/${id}/account_states`, { params: queryParams });
+    return response.data;
+  }
+
+  /**
+   * Get the alternative titles for a movie.
+   * @see https://developer.themoviedb.org/reference/movie-alternative-titles
+   */
+  async getAlternativeTitles(
+    id: number,
+    params?: MovieAlternativeTitlesParams
+  ): Promise<MovieAlternativeTitlesResponse> {
+    const queryParams: Record<string, any> = {};
+    if (params?.country) queryParams["country"] = params.country;
+
+    const response = await this.axiosInstance.get(`movie/${id}/alternative_titles`, { params: queryParams });
+    return response.data;
+  }
+
+  /**
+   * Get the recent changes for a movie.
+   * @see https://developer.themoviedb.org/reference/movie-changes
+   */
+  async getChanges(id: number, params?: MovieChangesParams): Promise<MovieChangesResponse> {
+    const queryParams: Record<string, any> = {};
+    if (params?.end_date) queryParams["end_date"] = params.end_date;
+    if (params?.page) queryParams["page"] = params.page;
+    if (params?.start_date) queryParams["start_date"] = params.start_date;
+
+    const response = await this.axiosInstance.get(`movie/${id}/changes`, { params: queryParams });
+    return response.data;
+  }
+
+  /**
+   * Get the cast and crew for a movie.
+   * @see https://developer.themoviedb.org/reference/movie-credits
+   */
+  async getCredits(id: number, params?: MovieCreditsParams): Promise<MovieCreditsResponse> {
+    const queryParams: Record<string, any> = {};
+    if (params?.language) queryParams["language"] = params.language;
+
+    const response = await this.axiosInstance.get(`movie/${id}/credits`, { params: queryParams });
+    return response.data;
+  }
+
+  /**
+   * Get the external IDs for a movie (IMDb, Facebook, etc.).
+   * @see https://developer.themoviedb.org/reference/movie-external-ids
+   */
+  async getExternalIds(id: number): Promise<MovieExternalIdsResponse> {
+    const response = await this.axiosInstance.get(`movie/${id}/external_ids`);
+    return response.data;
+  }
+
+  /**
+   * Get the images (backdrops, posters, logos) that belong to a movie.
+   * @see https://developer.themoviedb.org/reference/movie-images
+   */
+  async getImages(id: number, params?: MovieImagesParams): Promise<MovieImagesResponse> {
+    const queryParams: Record<string, any> = {};
+    if (params?.include_image_language) queryParams["include_image_language"] = params.include_image_language;
+    if (params?.language) queryParams["language"] = params.language;
+
+    const response = await this.axiosInstance.get(`movie/${id}/images`, { params: queryParams });
+    return response.data;
+  }
+
+  /**
+   * Get the keywords for a movie.
+   * @see https://developer.themoviedb.org/reference/movie-keywords
+   */
+  async getKeywords(id: number): Promise<MovieKeywordsResponse> {
+    const response = await this.axiosInstance.get(`movie/${id}/keywords`);
+    return response.data;
+  }
+
+  /**
+   * Get the newest movie ID.
+   * @see https://developer.themoviedb.org/reference/movie-latest-id
+   */
+  async getLatest(): Promise<MovieDetails> {
+    const response = await this.axiosInstance.get("movie/latest");
+    return response.data;
+  }
+
+  /**
+   * Get the lists that a movie has been added to.
+   * @see https://developer.themoviedb.org/reference/movie-lists
+   */
+  async getLists(id: number, params?: MovieListsParams): Promise<MovieListsResponse> {
+    const queryParams: Record<string, any> = {};
+    if (params?.language) queryParams["language"] = params.language;
+    if (params?.page) queryParams["page"] = params.page;
+
+    const response = await this.axiosInstance.get(`movie/${id}/lists`, { params: queryParams });
+    return response.data;
+  }
+
+  /**
+   * Get the recommended movies for a movie.
+   * @see https://developer.themoviedb.org/reference/movie-recommendations
+   */
+  async getRecommendations(
+    id: number,
+    params?: MovieRecommendationsParams
+  ): Promise<MovieListResponse> {
+    const queryParams: Record<string, any> = {};
+    if (params?.language) queryParams["language"] = params.language;
+    if (params?.page) queryParams["page"] = params.page;
+
+    const response = await this.axiosInstance.get(`movie/${id}/recommendations`, { params: queryParams });
+    return response.data;
+  }
+
+  /**
+   * Get the release dates and certifications for a movie.
+   * @see https://developer.themoviedb.org/reference/movie-release-dates
+   */
+  async getReleaseDates(id: number): Promise<MovieReleaseDatesResponse> {
+    const response = await this.axiosInstance.get(`movie/${id}/release_dates`);
+    return response.data;
+  }
+
+  /**
+   * Get the user reviews for a movie.
+   * @see https://developer.themoviedb.org/reference/movie-reviews
+   */
+  async getReviews(id: number, params?: MovieReviewsParams): Promise<MovieReviewsResponse> {
+    const queryParams: Record<string, any> = {};
+    if (params?.language) queryParams["language"] = params.language;
+    if (params?.page) queryParams["page"] = params.page;
+
+    const response = await this.axiosInstance.get(`movie/${id}/reviews`, { params: queryParams });
+    return response.data;
+  }
+
+  /**
+   * Get the similar movies based on genres and keywords.
+   * @see https://developer.themoviedb.org/reference/movie-similar
+   */
+  async getSimilar(
+    id: number,
+    params?: MovieSimilarParams
+  ): Promise<MovieListResponse> {
+    const queryParams: Record<string, any> = {};
+    if (params?.language) queryParams["language"] = params.language;
+    if (params?.page) queryParams["page"] = params.page;
+
+    const response = await this.axiosInstance.get(`movie/${id}/similar`, { params: queryParams });
+    return response.data;
+  }
+
+  /**
+   * Get the translations for a movie.
+   * @see https://developer.themoviedb.org/reference/movie-translations
+   */
+  async getTranslations(id: number): Promise<MovieTranslationsResponse> {
+    const response = await this.axiosInstance.get(`movie/${id}/translations`);
+    return response.data;
+  }
+
+  /**
+   * Get the videos that have been added to a movie.
+   * @see https://developer.themoviedb.org/reference/movie-videos
+   */
+  async getVideos(id: number, params?: MovieVideosParams): Promise<MovieVideosResponse> {
+    const queryParams: Record<string, any> = {};
+    if (params?.language) queryParams["language"] = params.language;
+
+    const response = await this.axiosInstance.get(`movie/${id}/videos`, { params: queryParams });
+    return response.data;
+  }
+
+  /**
+   * Get the list of streaming providers for a movie.
+   * @see https://developer.themoviedb.org/reference/movie-watch-providers
+   */
+  async getWatchProviders(id: number): Promise<MovieWatchProvidersResponse> {
+    const response = await this.axiosInstance.get(`movie/${id}/watch/providers`);
+    return response.data;
+  }
+
+  /**
+   * Rate a movie and save it to your rated list.
+   * @see https://developer.themoviedb.org/reference/movie-add-rating
+   */
+  async addRating(
+    id: number,
+    params: AddRatingParams,
+    request: AddRatingRequest
+  ): Promise<TMDBResponse> {
+    const queryParams: Record<string, any> = {};
+    if (params.session_id) queryParams["session_id"] = params.session_id;
+    if (params.guest_session_id) queryParams["guest_session_id"] = params.guest_session_id;
+
+    const response = await this.axiosInstance.post(
+      `movie/${id}/rating`,
+      request,
+      { params: queryParams }
+    );
+    return response.data;
+  }
+
+  /**
+   * Delete a user rating for a movie.
+   * @see https://developer.themoviedb.org/reference/movie-delete-rating
+   */
+  async deleteRating(
+    id: number,
+    params: DeleteRatingParams
+  ): Promise<TMDBResponse> {
+    const queryParams: Record<string, any> = {};
+    if (params.session_id) queryParams["session_id"] = params.session_id;
+    if (params.guest_session_id) queryParams["guest_session_id"] = params.guest_session_id;
+
+    const response = await this.axiosInstance.delete(
+      `movie/${id}/rating`,
+      { params: queryParams }
+    );
     return response.data;
   }
 }
--- src/types/movie.ts
+++ src/types/movie.ts
@@ -38,3 +38,364 @@ export type NowPlayingParams = MovieListParams;
 export type PopularParams = MovieListParams;
 export type TopRatedParams = MovieListParams;
 export type UpcomingParams = MovieListParams;
+
+export interface BelongsToCollection {
+  id: number;
+  name: string;
+  poster_path: string | null;
+  backdrop_path: string | null;
+}
+
+export interface Genre {
+  id: number;
+  name: string;
+}
+
+export interface ProductionCompany {
+  id: number;
+  logo_path: string | null;
+  name: string;
+  origin_country: string;
+}
+
+export interface ProductionCountry {
+  iso_3166_1: string;
+  name: string;
+}
+
+export interface SpokenLanguage {
+  english_name: string;
+  iso_639_1: string;
+  name: string;
+}
+
+export interface MovieDetails {
+  adult: boolean;
+  backdrop_path: string | null;
+  belongs_to_collection: BelongsToCollection | null;
+  budget: number;
+  genres: Genre[];
+  homepage: string | null;
+  id: number;
+  imdb_id: string | null;
+  origin_country: string[];
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string | null;
+  production_companies: ProductionCompany[];
+  production_countries: ProductionCountry[];
+  release_date: string;
+  revenue: number;
+  runtime: number;
+  spoken_languages: SpokenLanguage[];
+  status: string;
+  tagline: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
+
+export interface MovieDetailsParams {
+  append_to_response?: string;
+  language?: string;
+}
+
+export interface MovieAccountStates {
+  id: number;
+  favorite: boolean;
+  rated: { value: number } | null;
+  watchlist: boolean;
+}
+
+export interface MovieAccountStatesParams {
+  session_id?: string;
+  guest_session_id?: string;
+}
+
+export interface AlternativeTitle {
+  iso_3166_1: string;
+  title: string;
+  type: string;
+}
+
+export interface MovieAlternativeTitlesResponse {
+  id: number;
+  titles: AlternativeTitle[];
+}
+
+export interface MovieAlternativeTitlesParams {
+  country?: string;
+}
+
+export interface MovieChangeItem {
+  id: string;
+  action: string;
+  time: string;
+  iso_639_1: string;
+  iso_3166_1: string;
+  value: Record<string, unknown>;
+}
+
+export interface MovieChangeGroup {
+  key: string;
+  items: MovieChangeItem[];
+}
+
+export interface MovieChangesResponse {
+  changes: MovieChangeGroup[];
+}
+
+export interface MovieChangesParams {
+  end_date?: string;
+  page?: number;
+  start_date?: string;
+}
+
+export interface CastMember {
+  adult: boolean;
+  gender: number;
+  id: number;
+  known_for_department: string;
+  name: string;
+  original_name: string;
+  popularity: number;
+  profile_path: string | null;
+  cast_id: number;
+  character: string;
+  credit_id: string;
+  order: number;
+}
+
+export interface CrewMember {
+  adult: boolean;
+  gender: number;
+  id: number;
+  known_for_department: string;
+  name: string;
+  original_name: string;
+  popularity: number;
+  profile_path: string | null;
+  credit_id: string;
+  department: string;
+  job: string;
+}
+
+export interface MovieCreditsResponse {
+  id: number;
+  cast: CastMember[];
+  crew: CrewMember[];
+}
+
+export interface MovieCreditsParams {
+  language?: string;
+}
+
+export interface MovieExternalIdsResponse {
+  id: number;
+  imdb_id: string | null;
+  wikidata_id: string | null;
+  facebook_id: string | null;
+  instagram_id: string | null;
+  twitter_id: string | null;
+}
+
+export interface ImageData {
+  aspect_ratio: number;
+  height: number;
+  iso_639_1: string | null;
+  file_path: string;
+  vote_average: number;
+  vote_count: number;
+  width: number;
+}
+
+export interface MovieImagesResponse {
+  backdrops: ImageData[];
+  id: number;
+  logos: ImageData[];
+  posters: ImageData[];
+}
+
+export interface MovieImagesParams {
+  include_image_language?: string;
+  language?: string;
+}
+
+export interface Keyword {
+  id: number;
+  name: string;
+}
+
+export interface MovieKeywordsResponse {
+  id: number;
+  keywords: Keyword[];
+}
+
+export interface MovieListEntry {
+  description: string;
+  favorite_count: number;
+  id: number;
+  item_count: number;
+  iso_639_1: string;
+  list_type: string;
+  name: string;
+  poster_path: string | null;
+}
+
+export interface MovieListsResponse {
+  id: number;
+  page: number;
+  results: MovieListEntry[];
+  total_pages: number;
+  total_results: number;
+}
+
+export interface MovieListsParams {
+  language?: string;
+  page?: number;
+}
+
+export interface MovieRecommendationsParams {
+  language?: string;
+  page?: number;
+}
+
+export interface ReleaseDateInfo {
+  certification: string;
+  descriptors: unknown[];
+  iso_639_1: string;
+  note: string;
+  release_date: string;
+  type: number;
+}
+
+export interface ReleaseDateCountry {
+  iso_3166_1: string;
+  release_dates: ReleaseDateInfo[];
+}
+
+export interface MovieReleaseDatesResponse {
+  id: number;
+  results: ReleaseDateCountry[];
+}
+
+export interface AuthorDetails {
+  name: string;
+  username: string;
+  avatar_path: string | null;
+  rating: number | null;
+}
+
+export interface Review {
+  author: string;
+  author_details: AuthorDetails;
+  content: string;
+  created_at: string;
+  id: string;
+  updated_at: string;
+  url: string;
+}
+
+export interface MovieReviewsResponse {
+  id: number;
+  page: number;
+  results: Review[];
+  total_pages: number;
+  total_results: number;
+}
+
+export interface MovieReviewsParams {
+  language?: string;
+  page?: number;
+}
+
+export interface MovieSimilarParams {
+  language?: string;
+  page?: number;
+}
+
+export interface TranslationData {
+  homepage: string;
+  overview: string;
+  runtime: number;
+  tagline: string;
+  title: string;
+}
+
+export interface Translation {
+  iso_3166_1: string;
+  iso_639_1: string;
+  name: string;
+  english_name: string;
+  data: TranslationData;
+}
+
+export interface MovieTranslationsResponse {
+  id: number;
+  translations: Translation[];
+}
+
+export interface Video {
+  iso_639_1: string;
+  iso_3166_1: string;
+  name: string;
+  key: string;
+  site: string;
+  size: number;
+  type: string;
+  official: boolean;
+  published_at: string;
+  id: string;
+}
+
+export interface MovieVideosResponse {
+  id: number;
+  results: Video[];
+}
+
+export interface MovieVideosParams {
+  language?: string;
+}
+
+export interface WatchProvider {
+  logo_path: string;
+  provider_id: number;
+  provider_name: string;
+  display_priority: number;
+}
+
+export interface CountryWatchProviders {
+  link: string;
+  flatrate?: WatchProvider[];
+  rent?: WatchProvider[];
+  buy?: WatchProvider[];
+  ads?: WatchProvider[];
+  free?: WatchProvider[];
+}
+
+export interface MovieWatchProvidersResponse {
+  id: number;
+  results: Record<string, CountryWatchProviders>;
+}
+
+export interface AddRatingRequest {
+  value: number;
+}
+
+export interface AddRatingParams {
+  session_id?: string;
+  guest_session_id?: string;
+}
+
+export interface DeleteRatingParams {
+  session_id?: string;
+  guest_session_id?: string;
+}
+
+export interface TMDBResponse {
+  status_code: number;
+  status_message: string;
+}
