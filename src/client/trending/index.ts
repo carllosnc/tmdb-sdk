6912ea1bc38ee3b9a21e19a396d9c36df052cea8
Carlos Costa <carllos.nc@gmail.com>
2026-05-23 14:41:35 -0300

    feat(trending): add TrendingClient and trending types
    - 4 methods: getAll, getMovies, getPeople, getTvShows
    - TimeWindow type ('day' | 'week') with default 'day'
    - Discriminated union for trending/all results (movie/tv/person)

---
+++ src/client/trending/index.ts
@@ -0,0 +1,72 @@
+import { type AxiosInstance } from "axios";
+import {
+  type TrendingAllResponse,
+  type TrendingMoviesResponse,
+  type TrendingPeopleResponse,
+  type TrendingParams,
+  type TrendingTvResponse,
+} from "../../types/trending.ts";
+
+export class TrendingClient {
+  constructor(private axiosInstance: AxiosInstance) {}
+
+  /**
+   * Get the trending movies, TV shows and people.
+   * @see https://developer.themoviedb.org/reference/trending-all
+   */
+  async getAll(params?: TrendingParams): Promise<TrendingAllResponse> {
+    const queryParams: Record<string, any> = {};
+    const timeWindow = params?.timeWindow ?? "day";
+    if (params?.language) queryParams["language"] = params.language;
+
+    const response = await this.axiosInstance.get(`trending/all/${timeWindow}`, {
+      params: queryParams,
+    });
+    return response.data;
+  }
+
+  /**
+   * Get the trending movies on TMDB.
+   * @see https://developer.themoviedb.org/reference/trending-movies
+   */
+  async getMovies(params?: TrendingParams): Promise<TrendingMoviesResponse> {
+    const queryParams: Record<string, any> = {};
+    const timeWindow = params?.timeWindow ?? "day";
+    if (params?.language) queryParams["language"] = params.language;
+
+    const response = await this.axiosInstance.get(`trending/movie/${timeWindow}`, {
+      params: queryParams,
+    });
+    return response.data;
+  }
+
+  /**
+   * Get the trending people on TMDB.
+   * @see https://developer.themoviedb.org/reference/trending-people
+   */
+  async getPeople(params?: TrendingParams): Promise<TrendingPeopleResponse> {
+    const queryParams: Record<string, any> = {};
+    const timeWindow = params?.timeWindow ?? "day";
+    if (params?.language) queryParams["language"] = params.language;
+
+    const response = await this.axiosInstance.get(`trending/person/${timeWindow}`, {
+      params: queryParams,
+    });
+    return response.data;
+  }
+
+  /**
+   * Get the trending TV shows on TMDB.
+   * @see https://developer.themoviedb.org/reference/trending-tv
+   */
+  async getTvShows(params?: TrendingParams): Promise<TrendingTvResponse> {
+    const queryParams: Record<string, any> = {};
+    const timeWindow = params?.timeWindow ?? "day";
+    if (params?.language) queryParams["language"] = params.language;
+
+    const response = await this.axiosInstance.get(`trending/tv/${timeWindow}`, {
+      params: queryParams,
+    });
+    return response.data;
+  }
+}
---
+++ src/types/trending.ts
@@ -0,0 +1,108 @@
+import type { MovieResult } from "./movie.ts";
+import type { PersonResult } from "./person.ts";
+
+export type TimeWindow = "day" | "week";
+
+export interface TrendingTvResult {
+  adult: boolean;
+  backdrop_path: string | null;
+  genre_ids: number[];
+  id: number;
+  origin_country: string[];
+  original_language: string;
+  original_name: string;
+  overview: string;
+  popularity: number;
+  poster_path: string | null;
+  first_air_date: string;
+  name: string;
+  vote_average: number;
+  vote_count: number;
+}
+
+export interface TrendingMovieResult extends MovieResult {}
+
+export interface TrendingPersonResult extends PersonResult {}
+
+export interface TrendingAllMovieResult {
+  adult: boolean;
+  backdrop_path: string | null;
+  genre_ids: number[];
+  id: number;
+  media_type: "movie";
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string | null;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
+
+export interface TrendingAllTvResult {
+  adult: boolean;
+  backdrop_path: string | null;
+  genre_ids: number[];
+  id: number;
+  media_type: "tv";
+  origin_country: string[];
+  original_language: string;
+  original_name: string;
+  overview: string;
+  popularity: number;
+  poster_path: string | null;
+  first_air_date: string;
+  name: string;
+  vote_average: number;
+  vote_count: number;
+}
+
+export interface TrendingAllPersonResult {
+  adult: boolean;
+  gender: number;
+  id: number;
+  known_for: { adult?: boolean; backdrop_path?: string | null; genre_ids: number[]; id: number; media_type: string; original_language: string; original_title?: string; original_name?: string; overview: string; poster_path?: string | null; release_date?: string; first_air_date?: string; title?: string; name?: string; video?: boolean; vote_average: number; vote_count: number }[];
+  known_for_department: string;
+  media_type: "person";
+  name: string;
+  popularity: number;
+  profile_path: string | null;
+}
+
+export type TrendingAllResult = TrendingAllMovieResult | TrendingAllTvResult | TrendingAllPersonResult;
+
+export interface TrendingAllResponse {
+  page: number;
+  results: TrendingAllResult[];
+  total_pages: number;
+  total_results: number;
+}
+
+export interface TrendingMoviesResponse {
+  page: number;
+  results: TrendingMovieResult[];
+  total_pages: number;
+  total_results: number;
+}
+
+export interface TrendingPeopleResponse {
+  page: number;
+  results: TrendingPersonResult[];
+  total_pages: number;
+  total_results: number;
+}
+
+export interface TrendingTvResponse {
+  page: number;
+  results: TrendingTvResult[];
+  total_pages: number;
+  total_results: number;
+}
+
+export interface TrendingParams {
+  language?: string;
+  timeWindow?: TimeWindow;
+}
