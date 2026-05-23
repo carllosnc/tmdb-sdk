62081a1ce923af1f9b2f586107073f623e117661
Carlos Costa <carllos.nc@gmail.com>
2026-05-23 14:29:57 -0300

    feat(person): add 10 new endpoints for person details, credits, images, translations
    Adds all person namespace endpoints:
    - GET /3/person/{id}
    - GET /3/person/{id}/changes
    - GET /3/person/{id}/combined_credits
    - GET /3/person/{id}/external_ids
    - GET /3/person/{id}/images
    - GET /3/person/latest
    - GET /3/person/{id}/movie_credits
    - GET /3/person/{id}/tv_credits
    - GET /3/person/{id}/tagged_images
    - GET /3/person/{id}/translations

--- src/client/person/index.ts
+++ src/client/person/index.ts
@@ -2,6 +2,21 @@ import { type AxiosInstance } from "axios";
 import {
   type PopularPersonResponse,
   type PopularPersonParams,
+  type PersonDetails,
+  type PersonDetailsParams,
+  type PersonChangesResponse,
+  type PersonChangesParams,
+  type CombinedCreditsResponse,
+  type CombinedCreditsParams,
+  type PersonExternalIds,
+  type PersonImagesResponse,
+  type PersonMovieCreditsResponse,
+  type PersonMovieCreditsParams,
+  type PersonTvCreditsResponse,
+  type PersonTvCreditsParams,
+  type PersonTaggedImagesResponse,
+  type PersonTaggedImagesParams,
+  type PersonTranslationsResponse,
 } from "../../types/person.ts";
 
 export class PersonClient {
@@ -13,6 +28,96 @@ export class PersonClient {
    */
   async getPopular(params?: PopularPersonParams): Promise<PopularPersonResponse> {
     const response = await this.axiosInstance.get("person/popular", { params });
+    return response.data;
+  }
+
+  /**
+   * Query the top level details of a person.
+   * @see https://developer.themoviedb.org/reference/person-details
+   */
+  async getDetails(personId: number, params?: PersonDetailsParams): Promise<PersonDetails> {
+    const response = await this.axiosInstance.get(`person/${personId}`, { params });
+    return response.data;
+  }
+
+  /**
+   * Get the recent changes for a person.
+   * @see https://developer.themoviedb.org/reference/person-changes
+   */
+  async getChanges(personId: number, params?: PersonChangesParams): Promise<PersonChangesResponse> {
+    const response = await this.axiosInstance.get(`person/${personId}/changes`, { params });
+    return response.data;
+  }
+
+  /**
+   * Get the combined movie and TV credits that belong to a person.
+   * @see https://developer.themoviedb.org/reference/person-combined-credits
+   */
+  async getCombinedCredits(personId: number, params?: CombinedCreditsParams): Promise<CombinedCreditsResponse> {
+    const response = await this.axiosInstance.get(`person/${personId}/combined_credits`, { params });
+    return response.data;
+  }
+
+  /**
+   * Get the external ID's that belong to a person.
+   * @see https://developer.themoviedb.org/reference/person-external-ids
+   */
+  async getExternalIds(personId: number): Promise<PersonExternalIds> {
+    const response = await this.axiosInstance.get(`person/${personId}/external_ids`);
+    return response.data;
+  }
+
+  /**
+   * Get the profile images that belong to a person.
+   * @see https://developer.themoviedb.org/reference/person-images
+   */
+  async getImages(personId: number): Promise<PersonImagesResponse> {
+    const response = await this.axiosInstance.get(`person/${personId}/images`);
+    return response.data;
+  }
+
+  /**
+   * Get the newest created person.
+   * @see https://developer.themoviedb.org/reference/person-latest-id
+   */
+  async getLatest(): Promise<PersonDetails> {
+    const response = await this.axiosInstance.get("person/latest");
+    return response.data;
+  }
+
+  /**
+   * Get the movie credits for a person.
+   * @see https://developer.themoviedb.org/reference/person-movie-credits
+   */
+  async getMovieCredits(personId: number, params?: PersonMovieCreditsParams): Promise<PersonMovieCreditsResponse> {
+    const response = await this.axiosInstance.get(`person/${personId}/movie_credits`, { params });
+    return response.data;
+  }
+
+  /**
+   * Get the TV credits that belong to a person.
+   * @see https://developer.themoviedb.org/reference/person-tv-credits
+   */
+  async getTvCredits(personId: number, params?: PersonTvCreditsParams): Promise<PersonTvCreditsResponse> {
+    const response = await this.axiosInstance.get(`person/${personId}/tv_credits`, { params });
+    return response.data;
+  }
+
+  /**
+   * Get the tagged images for a person.
+   * @see https://developer.themoviedb.org/reference/person-tagged-images
+   */
+  async getTaggedImages(personId: number, params?: PersonTaggedImagesParams): Promise<PersonTaggedImagesResponse> {
+    const response = await this.axiosInstance.get(`person/${personId}/tagged_images`, { params });
+    return response.data;
+  }
+
+  /**
+   * Get the translations that belong to a person.
+   * @see https://developer.themoviedb.org/reference/translations
+   */
+  async getTranslations(personId: number): Promise<PersonTranslationsResponse> {
+    const response = await this.axiosInstance.get(`person/${personId}/translations`);
     return response.data;
   }
 }
--- src/types/person.ts
+++ src/types/person.ts
@@ -40,3 +40,302 @@ export interface PopularPersonParams {
   language?: string;
   page?: number;
 }
+
+export interface PersonDetails {
+  adult: boolean;
+  also_known_as: string[];
+  biography: string;
+  birthday: string | null;
+  deathday: string | null;
+  gender: number;
+  homepage: string | null;
+  id: number;
+  imdb_id: string | null;
+  known_for_department: string;
+  name: string;
+  place_of_birth: string | null;
+  popularity: number;
+  profile_path: string | null;
+}
+
+export interface PersonDetailsParams {
+  append_to_response?: string;
+  language?: string;
+}
+
+export interface PersonChangeItem {
+  id: string;
+  action: string;
+  time: string;
+  iso_639_1: string;
+  iso_3166_1: string;
+  value: string;
+}
+
+export interface PersonChange {
+  key: string;
+  items: PersonChangeItem[];
+}
+
+export interface PersonChangesResponse {
+  changes: PersonChange[];
+}
+
+export interface PersonChangesParams {
+  end_date?: string;
+  page?: number;
+  start_date?: string;
+}
+
+export interface MovieCastCredit {
+  adult: boolean;
+  backdrop_path: string | null;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string | null;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+  character: string;
+  credit_id: string;
+  order: number;
+}
+
+export interface MovieCrewCredit {
+  adult: boolean;
+  backdrop_path: string | null;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string | null;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+  credit_id: string;
+  department: string;
+  job: string;
+}
+
+export interface PersonMovieCreditsResponse {
+  id: number;
+  cast: MovieCastCredit[];
+  crew: MovieCrewCredit[];
+}
+
+export interface PersonMovieCreditsParams {
+  language?: string;
+}
+
+export interface TvCastCredit {
+  adult: boolean;
+  backdrop_path: string | null;
+  genre_ids: number[];
+  id: number;
+  origin_country: string[];
+  original_language: string;
+  original_name: string;
+  overview: string;
+  popularity: number;
+  poster_path: string | null;
+  first_air_date: string;
+  name: string;
+  vote_average: number;
+  vote_count: number;
+  character: string;
+  credit_id: string;
+  episode_count: number;
+}
+
+export interface TvCrewCredit {
+  adult: boolean;
+  backdrop_path: string | null;
+  genre_ids: number[];
+  id: number;
+  origin_country: string[];
+  original_language: string;
+  original_name: string;
+  overview: string;
+  popularity: number;
+  poster_path: string | null;
+  first_air_date: string;
+  name: string;
+  vote_average: number;
+  vote_count: number;
+  credit_id: string;
+  department: string;
+  episode_count: number;
+  job: string;
+}
+
+export interface PersonTvCreditsResponse {
+  id: number;
+  cast: TvCastCredit[];
+  crew: TvCrewCredit[];
+}
+
+export interface PersonTvCreditsParams {
+  language?: string;
+}
+
+export interface CombinedCreditCast {
+  adult: boolean;
+  backdrop_path: string | null;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title?: string;
+  original_name?: string;
+  overview: string;
+  popularity: number;
+  poster_path: string | null;
+  release_date?: string;
+  first_air_date?: string;
+  title?: string;
+  name?: string;
+  video?: boolean;
+  vote_average: number;
+  vote_count: number;
+  character: string;
+  credit_id: string;
+  order: number;
+  media_type: string;
+}
+
+export interface CombinedCreditCrew {
+  adult: boolean;
+  backdrop_path: string | null;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title?: string;
+  original_name?: string;
+  overview: string;
+  popularity: number;
+  poster_path: string | null;
+  release_date?: string;
+  first_air_date?: string;
+  title?: string;
+  name?: string;
+  video?: boolean;
+  vote_average: number;
+  vote_count: number;
+  credit_id: string;
+  department: string;
+  job: string;
+  media_type: string;
+}
+
+export interface CombinedCreditsResponse {
+  id: number;
+  cast: CombinedCreditCast[];
+  crew: CombinedCreditCrew[];
+}
+
+export interface CombinedCreditsParams {
+  language?: string;
+}
+
+export interface PersonExternalIds {
+  id: number;
+  freebase_mid: string | null;
+  freebase_id: string | null;
+  imdb_id: string | null;
+  tvrage_id: number | null;
+  wikidata_id: string | null;
+  facebook_id: string | null;
+  instagram_id: string | null;
+  tiktok_id: string | null;
+  twitter_id: string | null;
+  youtube_id: string | null;
+}
+
+export interface PersonProfile {
+  aspect_ratio: number;
+  height: number;
+  iso_639_1: string | null;
+  file_path: string;
+  vote_average: number;
+  vote_count: number;
+  width: number;
+}
+
+export interface PersonImagesResponse {
+  id: number;
+  profiles: PersonProfile[];
+}
+
+export interface TaggedImageMedia {
+  adult?: boolean;
+  backdrop_path?: string | null;
+  id: number;
+  title?: string;
+  name?: string;
+  original_language?: string;
+  original_title?: string;
+  original_name?: string;
+  overview?: string;
+  poster_path?: string | null;
+  media_type: string;
+  genre_ids?: number[];
+  popularity?: number;
+  release_date?: string;
+  first_air_date?: string;
+  video?: boolean;
+  vote_average?: number;
+  vote_count?: number;
+}
+
+export interface TaggedImage {
+  aspect_ratio: number;
+  file_path: string;
+  height: number;
+  id: string;
+  iso_639_1: string | null;
+  vote_average: number;
+  vote_count: number;
+  width: number;
+  image_type: string;
+  media: TaggedImageMedia;
+  media_type: string;
+}
+
+export interface PersonTaggedImagesResponse {
+  id: number;
+  page: number;
+  results: TaggedImage[];
+  total_pages: number;
+  total_results: number;
+}
+
+export interface PersonTaggedImagesParams {
+  page?: number;
+}
+
+export interface PersonTranslationData {
+  biography: string;
+  name: string;
+}
+
+export interface PersonTranslation {
+  iso_3166_1: string;
+  iso_639_1: string;
+  name: string;
+  english_name: string;
+  data: PersonTranslationData;
+}
+
+export interface PersonTranslationsResponse {
+  id: number;
+  translations: PersonTranslation[];
+}
