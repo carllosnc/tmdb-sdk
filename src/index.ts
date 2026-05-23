bcc82bf3088516a9f7a6f3f466cdcc09f4b5fd47
Carlos Costa <carllos.nc@gmail.com>
2026-05-23 01:39:25 -0300

    feat: export authentication client and types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export { TMDBClient, type TMDBClientConfig } from "./client.ts";
 export { AccountClient } from "./client/account/index.ts";
+export { AuthenticationClient } from "./client/authentication/index.ts";
 export * from "./types/account.ts";
+export * from "./types/authentication.ts";
