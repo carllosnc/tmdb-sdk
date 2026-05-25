6fe7be5619d3a27f458b444809c282f4010cd08e
Carlos Costa <carllos.nc@gmail.com>
2026-05-24 21:28:37 -0300

    feat(v4): add auth client (request token, access token, logout)

---
+++ src/client/auth-v4/index.ts
@@ -0,0 +1,55 @@
+import { type AxiosInstance } from "axios";
+import {
+  type V4CreateAccessTokenRequest,
+  type V4CreateAccessTokenResponse,
+  type V4CreateRequestTokenParams,
+  type V4CreateRequestTokenResponse,
+  type V4LogoutRequest,
+  type V4LogoutResponse,
+} from "../../types/auth-v4.js";
+
+export class AuthV4Client {
+  constructor(private axiosInstance: AxiosInstance) {}
+
+  /**
+   * Generate a new request token to ask a user to approve.
+   * @see https://developer.themoviedb.org/v4/reference/auth-create-request-token
+   */
+  async createRequestToken(
+    params?: V4CreateRequestTokenParams
+  ): Promise<V4CreateRequestTokenResponse> {
+    const response = await this.axiosInstance.post(
+      "/4/auth/request_token",
+      params?.redirect_to ? { redirect_to: params.redirect_to } : undefined
+    );
+    return response.data;
+  }
+
+  /**
+   * Finish the user authentication flow and issue an official user access token.
+   * @see https://developer.themoviedb.org/v4/reference/auth-create-access-token
+   */
+  async createAccessToken(
+    request: V4CreateAccessTokenRequest
+  ): Promise<V4CreateAccessTokenResponse> {
+    const response = await this.axiosInstance.post(
+      "/4/auth/access_token",
+      request
+    );
+    return response.data;
+  }
+
+  /**
+   * Log out of a session by invalidating an access token.
+   * @see https://developer.themoviedb.org/v4/reference/auth-logout
+   */
+  async logout(
+    request: V4LogoutRequest
+  ): Promise<V4LogoutResponse> {
+    const response = await this.axiosInstance.delete(
+      "/4/auth/access_token",
+      { data: request }
+    );
+    return response.data;
+  }
+}
